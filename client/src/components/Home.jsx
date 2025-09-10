798b5e376463c1c1024cf56b768fe5a61577e469
sajid-25 <sajid.cse.20230104025@aust.edu>
2025-09-10 11:05:10 +0600

    Column

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-        this is home
+      <div class="row align-items-start border-1">
+        <div class="col">One of three columns</div>
+        <div class="col">One of three columns</div>
+        <div class="col">One of three columns</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
