baeba6fc2dcc26f844e51800939e4897da97b80e
fuad <nafisfuadisc@gmail.com>
2025-09-16 03:14:24 +0600

    Fix 1: comment

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -10,7 +10,6 @@ const postSchema = new mongoose.Schema(
       {
         author_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
         comment: { type: String, required: true },
-        required: true
       }, { timestamps: true }
     ],
   },
