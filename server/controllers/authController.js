3e46faed12df21b4139aa025d7e5f6632302726a
fuad <nafisfuadisc@gmail.com>
2025-09-16 07:10:31 +0600

    Rename userController to authController

---
+++ server/controllers/authController.js
@@ -0,0 +1,41 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+// generate JWT token
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "7d" });
+};
+
+// signup a user
+export const signup = async (req, res) => {
+  const { name, surname, email, password } = req.body;
+
+  try {
+    const user = await UserModel.register(name, surname, email, password);
+    const token = createToken(user._id);
+
+    res.status(200).json({ message: "User registered successfully", _id: user._id, email, token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// login a user
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+
+    res.status(200).json({ message: "Login successful", _id: user._id, email, token });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+// logout a user
+export const logout = (req, res) => {
+  // frontend to simply delete token from localStorage/cookies
+  res.status(200).json({ message: "Logout successful" });
+};
