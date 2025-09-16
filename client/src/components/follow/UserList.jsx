b5f21ffab2f4ee56e75027c92153a4b5b4583c52
sajid-25 <sajid.cse.20230104025@aust.edu>
2025-09-16 21:40:14 +0600

    Follow request sending

--- client/src/components/follow/UserList.jsx
+++ client/src/components/follow/UserList.jsx
@@ -1,6 +1,33 @@
 import { format, set } from "date-fns";
+import { useState } from "react";
+
 
 function UserList({ user }) {
+    const [isFollowing, setIsFollowing] = useState(false);
+
+    const handleFollow = async () => {
+    try {
+        const currentUser = JSON.parse(localStorage.getItem('user'));
+      const res = await fetch(`http://localhost:4000/api/follow/${user._id}`, {
+        method: "POST",
+        headers: {
+           Authorization: currentUser ? `Bearer ${currentUser.token}` : "",
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        console.error(data.error);
+        return;
+      }
+
+      setIsFollowing(true);
+    } catch (err) {
+      console.error("Failed to send follow request:", err);
+    }
+  };
+
   return (
     <>
       <div className="border d-flex gap-3 p-3 mb-4 align-items-center rounded">
@@ -24,8 +51,9 @@ function UserList({ user }) {
           <button
             className="border border-0 rounded"
             style={{ height: "40px", width: "100px" }}
+            onClick={handleFollow}
           >
-            Follow
+            {isFollowing ? "Unfollow" : "Follow"}
           </button>
         </div>
       </div>
